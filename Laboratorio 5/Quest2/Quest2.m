28fb6b026d66b01ab3e71271ef3f46bc895adbcd
Tomás Rosemberg (trosemberg) <tomasroro@hotmail.com>
2018-05-10 14:47:42 -0300

    arrumei a demora no tempo

--- Laboratorio 5/Quest2/Quest2.m
+++ Laboratorio 5/Quest2/Quest2.m
@@ -1,7 +1,7 @@
 clc;clear;
 %%histograma original fig_lista3_1
 img = imread('fig_lista3_1.png');
-histograma = zeros(256);
+histograma = zeros(1,256);
 oi = 0;
 tamimg = size(img);
 
@@ -11,13 +11,12 @@ for linha = 1:1:tamimg(1)
    histograma(oi) = histograma (oi) + 1;
  end
 end
-%eixo_x = 0:1:255;
-bar(histograma,1);
-saveas(gcf,'Q2_hist_fig.png')
+bar(histograma);
+saveas(gcf,'Q2_hist_fig.png');
 %%fim histograma original 
 %%histograma original einstein
 einst = imread('einsteinLowContrast.png');
-histeinst = zeros(256);
+histeinst = zeros(1,256);
 temp = 0;
 tamimg = size(einst);
 
@@ -28,9 +27,8 @@ for linha = 1:1:tamimg(1)
    histeinst(temp) = histeinst (temp) + 1;
  end
 end
-eixo_x = 0:1:255;
-bar(eixo_x,histeinst,1);
-saveas(gcf,'hist_eintein.png')
+bar(histeinst);
+saveas(gcf,'hist_eintein.png');
 %%fim histograma einstein
 
 %%equalizar os histogramas
