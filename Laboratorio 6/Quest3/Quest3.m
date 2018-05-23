49863ce415ebe1b2aedd4157d5e7ab1b03f8f649
Tomás Rosemberg (trosemberg) <tomasroro@hotmail.com>
2018-05-23 13:33:06 -0300

    Relatório 6 pronto

--- Laboratorio 6/Quest3/Quest3.m
+++ Laboratorio 6/Quest3/Quest3.m
@@ -7,30 +7,28 @@ new_vista = zeros(tamvista(1),tamvista(2),3);
 new_mundo(:,:,1) = mundo(:,:);
 new_mundo(:,:,2) = mundo(:,:);
 new_mundo(:,:,3) = mundo(:,:);
-lim_mundo=50;
-lim_mundo_m = 180;
 lim_vista=20;
 new_vista(:,:,1) = vista(:,:);
 new_vista(:,:,2) = vista(:,:);
 new_vista(:,:,3) = vista(:,:);
 for linha = 1:tammundo(1)
     for coluna = 1: tammundo(2)
-        if((mundo(linha,coluna)<30) && (linha<2*(tammundo(1)/3)))%preto->azul
+        if((mundo(linha,coluna)<27) && (linha<2*(tammundo(1)/3)))%preto->azul
             new_mundo(linha,coluna,1) = 51-1.5*mundo(linha,coluna);
             new_mundo(linha,coluna,2) = 204-1.5*mundo(linha,coluna);
             new_mundo(linha,coluna,3) = 255-1.5*mundo(linha,coluna);
-        elseif(mundo(linha,coluna)>245)%cinzando a parte branca do mapa
-            new_mundo(linha,coluna,1) = 230;
-            new_mundo(linha,coluna,2) = 230;
-            new_mundo(linha,coluna,3) = 230;
-        elseif((mundo(linha,coluna)<16) && (linha>2*(tammundo(1)/3)))%->tentando nao piintar argentina
+        elseif(mundo(linha,coluna)>210)%cinzando a parte branca do mapa
+            new_mundo(linha,coluna,1) = 210+mundo(linha,coluna)/210;
+            new_mundo(linha,coluna,2) = 210+mundo(linha,coluna)/210;
+            new_mundo(linha,coluna,3) = 210+mundo(linha,coluna)/210;
+        elseif((mundo(linha,coluna)<16) && (linha>2*(tammundo(1)/3)))%->tentando nao pintar argentina
             new_mundo(linha,coluna,1) = 51-1.5*mundo(linha,coluna);
             new_mundo(linha,coluna,2) = 204-1.5*mundo(linha,coluna);
             new_mundo(linha,coluna,3) = 255-1.5*mundo(linha,coluna);
-        elseif((mundo(linha,coluna)>100) && (mundo(linha,coluna)<255) && (linha>3*(tammundo(1)/4)))%->antartica
-            new_mundo(linha,coluna,1) = 130+255-mundo(linha,coluna);
-            new_mundo(linha,coluna,2) = 130+255-mundo(linha,coluna);
-            new_mundo(linha,coluna,3) = 130+255-mundo(linha,coluna);
+        elseif((mundo(linha,coluna)>100) && (mundo(linha,coluna)<240) && (linha>3*(tammundo(1)/4)))%->antartica
+            new_mundo(linha,coluna,1) = 1.3*mundo(linha,coluna);
+            new_mundo(linha,coluna,2) = 1.3*mundo(linha,coluna);
+            new_mundo(linha,coluna,3) = 1.3*mundo(linha,coluna);
         end
     end
 end
