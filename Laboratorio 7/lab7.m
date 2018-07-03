38135f53764e644005f147edbb88e1ce5adc1fac
Tomás Rosemberg (trosemberg) <tomasroro@hotmail.com>
2018-07-03 00:04:58 -0300

    relatorio 7

--- Laboratorio 7/lab7.m
+++ Laboratorio 7/lab7.m
@@ -4,7 +4,7 @@ img = imread('Parlamento_Britanico.png');
 fun = @dct2;
 imagem = (blkproc(img,[8 8],fun))/8;
 imshow(imagem);
-imwrite(imagem,'eae.png');
+imwrite(imagem,'DCT-2D.png');
 Q = [16 11 10 16 24 40 51 61;
     12 12 14 19 26 58 60 55;
     14 13 16 24 40 57 69 56;
@@ -16,18 +16,20 @@ Q = [16 11 10 16 24 40 51 61;
 if qualidade<50
     escala_quant = 50/quantidade;
 else
-    escala_quant = 2- (qualidade*2)/100;
+    escala_quant = 2 - (qualidade*2)/100;
 end
 QuantT = max(ones(size(Q)),round(escala_quant*Q));
 fun2 =@(X) round(X./QuantT);
 qualid50 = round(blkproc(imagem,[8 8],fun2));
-figure;imshow(uint8(qualid50));
-imwrite(uint8(qualid50),'qualid50.png');
 entropia_imagem = entropy(img);
-entropia_qualid = entropy(qualid50);
+entropia_qualid = entropy(uint8(qualid50));
 fun3 =@(X) round(X.*QuantT);
 recuperada = 8*blkproc(qualid50,[8 8],fun3);
 recuperada = blkproc(recuperada,[8 8],@idct2);
-figure;imshow(uint8(recuperada));
-imwrite(uint8(recuperada),'recuperada.png');
+figure;
+imshow(uint8(recuperada));
+imwrite(uint8(recuperada),'fig3_100.png');
+
+MSE = immse(uint8(img),uint8(recuperada));
+SSIM = ssim(uint8(img),uint8(recuperada));
 
